364bb245a0444732f45cfdb61d85b6c29e553ee4
LNRtopher <143825175+LNRtopher@users.noreply.github.com>
2024-01-05 15:41:33 -0800

    Co-authored-by: Hubbisand <Hubbisand@users.noreply.github.com>

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 //import AddGame from './AddGame.jsx'
-//import ShowGame from './ShowGame.jsx'
+import ShowGame from 'ShowGame.jsx'
 const App = () => {
 
         return (
             <div>
-                <h1>my game shelfie</h1>
+                <h1>this is my app</h1>
+                ShowGame
             </div>
+            
         )
 }
 
