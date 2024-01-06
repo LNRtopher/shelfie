d3e3ff06a79bcc2e554e1312c56688c5d72c3ea9
LNRtopher <143825175+LNRtopher@users.noreply.github.com>
2024-01-06 03:29:56 -0800

    connecting to db

--- server/server.js
+++ server/server.js
@@ -6,40 +6,15 @@ const mongoose = require('mongoose');
 const router = express.Router();
 //mongosh "mongodb+srv://new_user:J44rih6CcIVFjmYN@cluster0.eiuf1d9.mongodb.net/?retryWrites=true&w=majority" --apiVersion 6012 --username new_user
 
-const uri = "mongodb+srv://new_user:J44rih6CcIVFjmYN@cluster0.eiuf1d9.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://solo:soloProject@cluster0.eiuf1d9.mongodb.net/?retryWrites=true&w=majority";
 const Game = require("./models.js");
 //mongoimport --uri  mongodb+srv://new_user:<enter_password>@cluster0.eiuf1d9.mongodb.net/<enter_database_name>  --collection  <enter_collection_name>  --type  <enter JSON/CSV/TSV>   --file  <enter path/to/file>
 const db = mongoose.connect(uri, {
-    useNewUrlParser: true,
-    dbName: 'myGameShelf'
-})
-    .then(() => console.log('connected to mongo db.'))
-    .catch(err => console.log(err));
-Game.insertMany([
-    {name: "protoGame",
-    numPlayers: [1,4],
-    location: "closet",
-    playedWith: {Nina: "loved"}
-    },
-    {name: "betaGame",
-    numPlayers: [1,4],
-    location: "closet",
-    playedWith: {Nina: "loved"}
-    }
-])
-//  const directoryPath = path.join(__dirname, '/');
-//         fs.readFile(directoryPath+'/workflow.json', 'utf8', function (err, data) {
-//         if (err) throw err;
-//         console.log(data);
-//         var json = JSON.parse(data);
-//         var data = {texto: json};
-//         db.collection("workflows").insert(data, function(err, res)     
-//             {
-//             if (err) throw err;
-//             });
-//         });
+   dbName: 'myGameShelf'
+ })   .then(() => console.log('connected to mongo db.'))
+      .catch(err => console.log(err));
 
-
+//deliver root html file
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req,res) => {
